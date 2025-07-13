ab2faa149d9a26c758b3473bbaf79b69970d8490
Reaishma N <hayagreevar9018@gmail.com>
2025-07-13 12:22:16 +0530

    Update kanban-board.tsx

--- client/src/pages/kanban-board.tsx
+++ client/src/pages/kanban-board.tsx
@@ -1,1 +1,10 @@
+import KanbanBoard from "@/components/kanban/kanban-board";
+
+export default function KanbanBoardPage() {
+  return (
+    <div className="space-y-6">
+      <KanbanBoard />
+    </div>
+  );
+}
 
